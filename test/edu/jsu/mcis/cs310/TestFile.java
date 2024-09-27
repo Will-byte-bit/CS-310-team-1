6f295d195aca0a0ddd91efd14503a2eee1e5902f
William Saint <wsaint@stu.jsu.edu>
2024-09-27 18:11:03 -0500

    should run

---
+++ test/edu/jsu/mcis/cs310/TestFile.java
@@ -0,0 +1,43 @@
+
+package edu.jsu.mcis.cs310;
+
+import com.github.cliftonlabs.json_simple.*;
+import java.util.List;
+import java.util.Set;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class TestFile {
+
+
+
+    public void setUp() {
+
+     
+        
+    }
+        
+    
+    @Test
+    public void testJsonToCsv() {
+        
+        
+        
+        try {
+         
+            
+            assertEquals(true, true);
+            
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail();
+        }
+        
+    }
+    
+    
+ 
+    
+}
