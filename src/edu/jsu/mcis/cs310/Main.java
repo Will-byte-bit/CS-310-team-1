596f293cc5df2ffedde56f4f4b272302790ddd70
William Saint <wsaint@stu.jsu.edu>
2024-09-27 17:48:44 -0500

    First Commit

---
+++ src/edu/jsu/mcis/cs310/Main.java
@@ -0,0 +1,15 @@
+package edu.jsu.mcis.cs310;
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        
+        try {
+            
+          System.out.println("Testing");
+        }
+        catch (Exception e) { e.printStackTrace(); }
+            
+    }
+    
+}
